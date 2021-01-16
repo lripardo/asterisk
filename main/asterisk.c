73d96f8f490e177324d93c43e10dc3521cb8684f
ricardo <ricardo_ripardo@hotmail.com>
2021-01-16 15:34:21 -0300

    First commit by Ripardo Softwares

--- main/asterisk.c
+++ main/asterisk.c
@@ -303,6 +303,7 @@ int daemon(int, int);  /* defined in libresolv of all places */
 #define WELCOME_MESSAGE \
     ast_verbose("Asterisk %s, " COPYRIGHT_TAG "\n" \
                 "Created by Mark Spencer <markster@digium.com>\n" \
+                "And update by Ripardo Softwares"        \
                 "Asterisk comes with ABSOLUTELY NO WARRANTY; type 'core show warranty' for details.\n" \
                 "This is free software, with components licensed under the GNU General Public\n" \
                 "License version 2 and other licenses; you are welcome to redistribute it under\n" \
